aed79a4bddc02fe85069c42d843a2f5529b27427
unreal5050 <95564449+unreal5050@users.noreply.github.com>
2021-12-14 23:49:36 +0900

    Delete keyboards/nowpad directory

--- keyboards/nowpad/ds1307.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void ds1307_set_time(uint8_t y, uint8_t mo, uint8_t d, uint8_t h, uint8_t m, uint8_t s);
-void ds1307_get_time(uint8_t *y, uint8_t *mo, uint8_t *d, uint8_t *h, uint8_t *m, uint8_t *s);
