803a31b76ca5b1d0e993a81b722fac726b54dc49
unreal <95564449+unreal5050@users.noreply.github.com>
2022-01-24 19:05:50 +0900

    Add files via upload

---
+++ keyboards/nowpad/ws2812c.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+void Disp_0(uint8_t digit);
+void Disp_1(uint8_t digit);
+void Disp_2(uint8_t digit);
+void Disp_3(uint8_t digit);
+void Disp_4(uint8_t digit);
+void Disp_5(uint8_t digit);
+void Disp_6(uint8_t digit);
+void Disp_7(uint8_t digit);
+void Disp_8(uint8_t digit);
+void Disp_9(uint8_t digit);
+void Disp_DP_time(void);
+void Disp_background(uint8_t b_h, uint8_t b_s, uint8_t b_v);
